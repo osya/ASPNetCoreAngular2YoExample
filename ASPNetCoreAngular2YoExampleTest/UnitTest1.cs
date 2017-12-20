166ff32a56e1f7178b54a54f090435ef1eb46f12
Valeriy Osipov <info@vosipov.com>
2017-12-21 00:55:42 +0300

    Fix OpenCover in appveyor.yml

--- ASPNetCoreAngular2YoExampleTest/UnitTest1.cs
+++ ASPNetCoreAngular2YoExampleTest/UnitTest1.cs
@@ -1,3 +1,4 @@
+using ASPNetCoreAngular2YoExample.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ASPNetCoreAngular2YoExampleTest
@@ -5,9 +6,15 @@ namespace ASPNetCoreAngular2YoExampleTest
     [TestClass]
     public class UnitTest1
     {
+        private readonly HomeController _homeController;
+
+        public UnitTest1() => _homeController = new HomeController();
+
         [TestMethod]
         public void TestMethod1()
         {
+            var result = _homeController.Index();
+            Assert.IsNotNull(result);
         }
     }
 }
